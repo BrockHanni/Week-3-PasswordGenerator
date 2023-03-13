47616f3e0620d9dffe89a4db84ab985da6bba54d
BrockHanni <hannibr26@gmail.com>
2023-03-13 03:33:14 -0600

    adding alerts

--- Develop/script.js
+++ Develop/script.js
@@ -5,10 +5,45 @@ var numbers = ["0","1","2","3","4","5","6","7","8","9"]
 var special = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+",";",":","[","]","{","}","?","<",">"]
 
 // adding the alerts to select characters to include
-var includeslower = confirm("Click OK to add lowercase Letters")
-var includesupper = confirm("Click OK to add UPPERCASE letters")
-var includesnumbers = confirm("Click OK to add numbers")
-var includesspecial = confirm("Click OK to add special characters")
+
+
+function passwordinfo() {
+  var length = parseInt(
+    prompt("How long would you like your password to be? (8-128")
+  );
+  if (length < 8) {
+    alert("Length must be between 8 & 128 characters")
+  }
+  if (length > 128){
+    alert("Length must be between 8 & 128 characters")
+  }
+
+  var includeslower = confirm("Click OK to add lowercase Letters")
+  var includesupper = confirm("Click OK to add UPPERCASE letters")
+  var includesnumbers = confirm("Click OK to add numbers")
+  var includesspecial = confirm("Click OK to add special characters")
+
+  if (
+    includeslower === false &
+    includesupper === false &
+    includesnumbers === false &
+    includesspecial === false   
+  ) {
+  alert("Select at least one character type")
+  }
+  var passwordinfo = {
+    length: length,
+    includeslower: includeslower,
+    includesupper: includesupper,
+    includesnumbers: includesnumbers,
+    includesspecial: includesspecial,
+  }
+  return passwordinfo;
+}
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
